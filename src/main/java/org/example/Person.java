6416bc2da30ef422f78afafa1863a2c84ecd03fb
IlsevNierop <ilsevannierop@gmail.com>
2023-03-24 15:29:31 +0100

    lege push naar main

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Person {
+
+//    De Person  heeft volgendeattributen:
+//
+//    name
+//            middleName
+//    lastName
+//            sex
+//    age
+//            mother
+//    father
+//            siblings
+//    children
+//            pets
+
+//    De Person bevat naast de attributen het volgende:
+//
+//    constructor met name, lastname, age en sex
+//    constructor met name, middleName, lastname, age en sex
+//    getters & setters
+//    addParents methode
+//    addChild methode
+//    addPet methode
+//    addSibling methode
+//    getGrandChildren methode
+
+}
---
+++ src/main/java/org/example/Pet.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Pet {
+
+//    De Pet heeft minimaal de volgende attributen:
+//
+//    name
+//            age
+//    species
+//            owner
+//
+//    De Pet bevat naast de attributen minimaal:
+//
+//    een constructor met alle attributen op 'owner' na
+//    getters & setters
+}
