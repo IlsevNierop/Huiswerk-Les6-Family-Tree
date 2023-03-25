67e4477662e4872ce4491f18461de2f670919c0b
IlsevNierop <ilsevannierop@gmail.com>
2023-03-25 15:16:30 +0100

    start gemaakt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,33 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person p = new Person ("Ilse", "van", "Nierop", 'F', 35);
+        Pet pet1 = new Pet("Gato", 4, "zwarte kat");
+
+
+        Person p1 = new Person ("Fred", "van", "Nierop", 'M', 63);
+        Person p2 = new Person ("Lena", "Garcia van Nierop", 'F', 2);
+        p2.addPet(pet1);
+
+        Person p3 = new Person ("Wessel", "van Nierop", 'M', 31);
+        Person p4 = new Person ("Louie", "van Nierop", 'M', 3);
+        Pet pet2 = new Pet("HondLouie", 6, "Golden Retriever");
+        p.addParents(p1);
+        p3.addChild(p4);
+        p3.addParents(p1);
+        p4.addPet(pet2);
+        p.addSibling(p3);
+
+        System.out.println(p.getFather().getName());
+        p2.addParents(p);
+
+        System.out.println(p2.getMother().getName());
+        p1.getPetsGrandChildren();
+
+        p.getChildren();
+
+        p.getNieces();
+
+
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,29 +1,185 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
 
-//    De Person  heeft volgendeattributen:
-//
-//    name
-//            middleName
-//    lastName
-//            sex
-//    age
-//            mother
-//    father
-//            siblings
-//    children
-//            pets
+    // get grandchildren methode
+    // get pets from grandchildren methode - moet nog returnen - nu alleen sout
+    // get all nieces from person methode - moet nog returnen (en check de setsex methode)
+    // Voeg een partner toe aan de Person en implementeer deze partner door de gehele applicatie inclusief getter & setter en benodigde methodes.
 
-//    De Person bevat naast de attributen het volgende:
-//
-//    constructor met name, lastname, age en sex
-//    constructor met name, middleName, lastname, age en sex
-//    getters & setters
-//    addParents methode
-//    addChild methode
-//    addPet methode
-//    addSibling methode
-//    getGrandChildren methode
 
+    private String name;
+    private String middleName;
+    private String lastName;
+    private char sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List<Person> siblings = new ArrayList<Person>();
+    private List<Person> children = new ArrayList<Person>();
+    private List<Pet> pets = new ArrayList<Pet>();
+
+    public Person(String name, String lastName, char sex, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Person(String name, String middleName, String lastName, char sex, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    private boolean hasUniqueName(Person person, List<Person> persons) {
+        if (persons.isEmpty()) {
+            return true;
+        }
+        for (Person p : persons) {
+            if (p.getName().equalsIgnoreCase(person.getName())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addSibling(Person sibling) {
+        if (hasUniqueName(sibling, siblings)) {
+            siblings.add(sibling);
+            sibling.addSibling(this);
+        }
+    }
+
+    public void addChild(Person child) {
+        if (hasUniqueName(child, children)) {
+            children.add(child);
+            child.addParents(this);
+        }
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+        pet.setOwner(this);
+    }
+
+    public List<Person> getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(List<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void addParents(Person p) {
+        if (p.getSex() == 'F' || p.getSex() == 'f' || p.getSex() == 'V' || p.getSex() == 'v') {
+            this.mother = p;
+        } else {
+            this.father = p;
+        }
+        p.addChild(this);
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    // check hoe goed op te lossen bij 'verkeerde' invoer
+    public void setSex(char sex) {
+        if (this.sex != 'F' || this.sex != 'M'){
+            this.sex = 'X';
+        }
+        else {
+            this.sex = sex;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    // nu print statement, maar moet returnen
+    public void getPetsGrandChildren() {
+        for (Person c : children){
+            for (Person child : c.getChildren()){
+                for (Pet pet : child.getPets()){
+                    System.out.println(child.getName() + " is a grandchild of " + this.name + " and has the following pets: ");
+                    System.out.println(pet.getName());
+                }
+
+            }
+        }
+
+    }
+
+    // nu print statement, maar moet returnen
+    public void getNieces() {
+        for (Person s : siblings){
+            for (Person childSibling : s.getChildren()){
+                if (childSibling.getSex() == 'F' || childSibling.getSex() == 'f' || childSibling.getSex() == 'V' || childSibling.getSex() == 'v'){
+                    System.out.println(childSibling.getName() + " is a niece of " + this.name);
+                }
+            }
+        }
+
+    }
 }
+
+
